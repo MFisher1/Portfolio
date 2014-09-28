5ab2c680671e18ef10979f4072c9b3ca43e2a08d
anthonymjones <anthony.mjones@gmail.com>
2014-09-28 17:29:55 -0600

    The Shell
    Basic format created, contact form completed, ready to customize.

---
+++ Portfolio/Portfolio/Controllers/ContactController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portfolio.Controllers
+{
+    public class ContactController : Controller
+    {
+        //
+        // GET: /Contact/
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(new Models.PortfolioContact());
+        }
+        [HttpPost]
+        public ActionResult Index(Models.PortfolioContact portfolioContact)
+        {
+            //create a connection to the database
+                Models.PortfolioEntities db = new Models.PortfolioEntities();
+            //add our contact form to the db
+                db.PortfolioContacts.Add(portfolioContact);
+            //save changes
+                db.SaveChanges();
+            //kick the user to the thank you page
+                return RedirectToAction("ThankYou", "Contact");
+        }
+        public ActionResult ThankYou()
+        {
+            return View();
+        }
+    }
+}
---
+++ Portfolio/Portfolio/Models/PortfolioContactValidation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Portfolio.Models
+{
+    [MetadataType(typeof(PortfolioContactValidation))]
+    public partial class PortfolioContact
+    {
+    }
+    public class PortfolioContactValidation
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required,EmailAddress]
+        public string Email { get; set; }
+        [Required,Phone]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public string Comment { get; set; }
+    }
+}
